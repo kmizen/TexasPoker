be94ba2b09539b76ce3ee892988d06f330492f37
kmizen <kevin.mizen@gmail.com>
2025-04-26 13:45:39 +0700

    Removed OpenCV folder from project

--- opencv/build/intermediates/cxx/Debug/43v6d6zh/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\kevin\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\kevin\\AndroidStudioProjects\\PokerAdvisor\\opencv\\.cxx\\Debug\\43v6d6zh\\arm64-v8a" ^
-  opencv_jni_shared
--- opencv/build/intermediates/cxx/Debug/43v6d6zh/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\kevin\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\kevin\\AndroidStudioProjects\\PokerAdvisor\\opencv\\.cxx\\Debug\\43v6d6zh\\armeabi-v7a" ^
-  opencv_jni_shared
